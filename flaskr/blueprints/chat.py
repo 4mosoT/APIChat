d94d04b2bd274412b154895514d7eaa4770cbc82
Marco Formoso <marco.a.formoso@gmail.com>
2023-04-03 15:44:45 +0200

    API selector

--- flaskr/blueprints/chat.py
+++ flaskr/blueprints/chat.py
@@ -10,7 +10,7 @@ def index():
         input = request.get_json()['question']
         answ = op.completion(input)
         return jsonify({'answer': answ['choices'][0]['message']['content']})
-    return render_template('chat/index.html', system=op.messages[0]["content"])
+    return render_template('chat/index.html', system=op.messages[0]["content"], current_model=op.current_model, models=op.models)
 
 @bp.route('/clear', methods=['GET'])
 def clear():
@@ -22,4 +22,12 @@ def change_system():
     new_system = request.get_json()['system']
     op.define_system(new_system)
     return jsonify({})
+
+@bp.route('/change_version', methods=['POST'])
+def change_version():
+    new_model = request.get_json()['version']
+    op.change_version(new_model)
+    print(op.current_model)
+    return jsonify({})
+
 
--- flaskr/openaiapi.py
+++ flaskr/openaiapi.py
@@ -2,21 +2,27 @@ import openai
 import os
 key = os.environ.get('openaikey')
 openai.api_key = key
-messages = [{"role": "system", "content": "You are a helpful assistant."}
-]
+messages = [{"role": "system", "content": "You are a helpful assistant."}]
 models = ["gpt-3.5-turbo","gpt-4"]
+current_model = models[0]
+
 def define_system(new_system):
     messages[0] = {"role": "system", "content": new_system}
 
 def completion(text):
+    print("Using model: " + current_model)
     messages.append({"role": "user", "content": text})
     r = openai.ChatCompletion.create(
-        model=models[0],
+        model=current_model,
         messages=messages,
     )
     messages.append({"role": "assistant", "content": r['choices'][0]['message']['content']})
+    print(messages)
     return r
 
 def clear():
     messages[1:] = []
-    
+
+def change_version(new_model):
+    global current_model 
+    current_model = new_model
